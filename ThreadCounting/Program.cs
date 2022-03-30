06ce8e1f88de36d15d42c40dc9eaa5fedf6b953a
Антон Анциферов <anton.antsiferov@vocord.ru>
2022-03-30 12:50:05 +0300

    ThreadedCounting solution

---
+++ ThreadCounting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ThreadCounting
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var c = new ThreadedCounting();
+            await c.Run();
+            c.Print();
+        }
+    }
+}
---
+++ ThreadCounting/ThreadedCounting.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadCounting
+{
+    public class ThreadedCounting
+    {
+        private ConcurrentDictionary<int, int> cd = new ConcurrentDictionary<int, int>();
+
+        public Task Run()
+        {
+            return Task.WhenAll(Count(1), Count(2), Count(3));
+
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Item - ThreadId");
+            foreach (var item in cd)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+        }
+
+        private Task Count(int stasrt)
+        {
+            return Task.Run(() =>
+            {
+                for (int i = stasrt; i <= 100; i += 3)
+                {
+                    cd.TryAdd(i, Thread.CurrentThread.ManagedThreadId);
+                }
+            });
+        }
+    }
+}
